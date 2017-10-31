75328093ef40238b1c54dafd3c2debff3b0e0399
Sadmir Halilovic <halilovic.sadmir@gmail.com>
2017-10-31 18:40:52 +0100

    added scaffolded spring boot project

---
+++ techday/src/main/java/se/rsv/umea/techday/TechdayApplication.java
@@ -0,0 +1,12 @@
+package se.rsv.umea.techday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechdayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechdayApplication.class, args);
+	}
+}
---
+++ techday/src/test/java/se/rsv/umea/techday/TechdayApplicationTests.java
@@ -0,0 +1,16 @@
+package se.rsv.umea.techday;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TechdayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
